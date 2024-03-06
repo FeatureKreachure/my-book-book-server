3f9c9e37165c8e80e7f12e4a21b193cc19f88c90
FeatureKreachure <43717996+FeatureKreachure@users.noreply.github.com>
2024-03-06 20:39:01 +0200

    removed commented code

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,12 +4,11 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // Consider hashing passwords
+    password: { type: String, required: true }, // hashed passwords
     interests: {
       type: [String],
       required: false,
     }, // Array of genres or topics the user is interested in
-    // You can add more fields as needed
   },
   { timestamps: true }
 );
